5ea62ec35df0f1f553388019e49ab0ba5bafb80f
mayankd271 <mayankdornala@gmail.com>
2021-04-11 18:11:30 -0500

    Update BackendInterface.java

--- BackendInterface.java
+++ BackendInterface.java
@@ -5,7 +5,12 @@
 // TA: Xinyi
 // Lecturer: Gary
 // Notes to Grader: Interface to define Backend function
+import java.util.List;
 public interface BackendInterface {
 	
-	 
+	 public List<String> shortestPath(LocationInterface src, LocationInterface destination);
+	 public List<String> returnAllLocation(); 
+	 public int shortestDistance(LocationInterface src, LocationInterface destination);
+
 }
+
