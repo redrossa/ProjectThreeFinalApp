62aead25c20ea1155e6457e9b9e98495b05b9ac5
boydlil <60155369+boydlil@users.noreply.github.com>
2021-04-12 19:50:33 -0500

    updating frontend from what it was

--- Frontend.java
+++ Frontend.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.List;
 
 // --== CS400 File Header Information ==--
 // Name: Lilly Boyd
@@ -22,7 +23,7 @@ public class Frontend {
    * @throws IOException
    */
   public void main(String[] args) throws IOException {
-    BackendDummy backend = new BackendDummy(args);
+    Backend backend = new Backend(args);
     // title screen for user:
     System.out.println("************************************");
     System.out.println("*                                  *");
@@ -30,11 +31,13 @@ public class Frontend {
     System.out.println("*                                  *");
     System.out.println("************************************");
     System.out.println("Press enter to continue: ");
+    System.out.println("------------------------------------");
     // try to proceed to next screen
     try {
       System.in.read();
     } catch (Exception e) {
       System.out.println("Please press enter to continue: ");
+      System.out.println("------------------------------------");
       main(args);
     }
     gpsMainMenu(backend);
@@ -46,7 +49,81 @@ public class Frontend {
    * 
    * @param back
    */
-  public static void gpsMainMenu(BackendDummy back) {
-
+  public static void gpsMainMenu(Backend back) {
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("              Main Menu             ");
+    System.out.println("------------------------------------");
+    System.out.println("Enter 1 to find the shortest distance between two cities.");
+    System.out.println("Enter 2 to view a list of the cities to travel to.");
+    System.out.println(
+        "Enter 3 to choose a city and view its neighboring cities and distances between them.");
+    System.out.println("Enter 0 to quit the GPS.");
+    try {
+      mode = userInput.nextInt();
+    } catch (InputMismatchException e) {
+      System.out.println("Invalid input, please enter an integer 0-3.");
+    }
+    // quit program
+    if (mode == 0) {
+      System.exit(0);
+    }
+    // shortest distance
+    if (mode == 1) {
+      // TODO: finish
+    }
+    // list of cities
+    else if (mode == 2) {
+      // TODO: finish
+    }
+    // city and neighbors
+    else if (mode == 3) {
+      // TODO: finish
+    } else {
+      System.out.println("Invalid input, please enter an integer 0-3.");
+      gpsMainMenu(back);
+    }
   }
+
+  /**
+   * Allows user to enter two cities from the list and find the shortest path of cities between them
+   * 
+   * @param backend
+   */
+  public static void getShortestPathBetweenTwoCities(Backend backend) {
+    startCity = "";
+    endCity = "";
+    Scanner input = new Scanner(System.in);
+    while (true) {
+      System.out.println("Find the Shortest Distance Between Two Cities:");
+      System.out.println("------------------------------------");
+      System.out.println("Cities to Choose from: ");
+      List<String> cities = backend.returnAllLocation();
+      for (int i = 0; i < cities.size(); i++) {
+        System.out.println(cities.get(i));
+      }
+      System.out.println("------------------------------------");
+      System.out.println("Please enter the name of the city you want to start at.");
+      try {
+        startCity = input.nextLine();
+      } catch (Exception e) {
+        System.out.println("Please enter a valid city. Try again.");
+        getShortestPathBetweenTwoCities(backend);
+      }
+      System.out.println("Please enter the city you want to end at.");
+      System.out.println("If you wish to return to the main menu, please enter 0");
+      try {
+        endCity = input.nextLine();
+      } catch (Exception e) {
+        System.out.println("Please enter a valid city. Try again.");
+        getShortestPathBetweenTwoCities(backend);
+      }
+      System.out.println("------------------------------------");
+      List<String> shortestPathToCities = backend.shortestPath(startCity, endCity);
+      System.out.println("The shortest path to take is: ");
+      for (int i = 0; i < shortestPathToCities.size(); i++) {
+        System.out.println(shortestPathToCities.get(i));
+      }
+    }
+  }
+
 }
